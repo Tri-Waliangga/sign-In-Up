fe53984ef74c48d2e6c239245d4d5d7205972f96
Tri-Waliangga <49595468+Tri-Waliangga@users.noreply.github.com>
2019-05-10 15:38:31 +0700

    add js file

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const container = document.getElementById('container');
+
+const addClass = () => container.classList.add('right-panel-active');
+
+const removeClass = () => container.classList.remove('right-panel-active');
